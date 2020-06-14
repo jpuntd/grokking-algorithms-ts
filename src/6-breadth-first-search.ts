b419c8a75f931a46e8384b68d8ff6269196d6632
Jan Dierckx <jpuntd@gmail.Com>
2020-06-14 22:09:32 +0200

    add show method to queue

--- src/6-breadth-first-search.ts
+++ src/6-breadth-first-search.ts
@@ -1,42 +1,52 @@
 class Queue<T> {
   private items: T[] = [];
-  
-  public enqueue= (item: T) => this.items.push(item);
+
+  public enqueue = (item: T) => this.items.push(item);
 
   public dequeue(): T {
-      return this.items.shift();
+    return this.items.shift();
   }
 
-  isEmpty():boolean {
-      return this.items.length === 0;
+  isEmpty(): boolean {
+    return this.items.length === 0;
+  }
+
+  public show(): void {
+    console.log(this.items);
   }
 }
 
 const friends = new Map<string, string[]>();
-friends.set('you',['bob', 'claire','alice']);
-friends.set('bob',['peggy', 'anuj']);
-friends.set('alice',['peggy']);
-friends.set('anuj',[]);
-friends.set('peggy',[]);
-friends.set('claire',['jonny','thom']);
-friends.set('thom',[]);
-friends.set('jonny',[]);
+friends.set("you", ["bob", "claire", "alice"]);
+friends.set("bob", ["peggy", "anuj"]);
+friends.set("alice", ["peggy"]);
+friends.set("anuj", []);
+friends.set("peggy", []);
+friends.set("claire", ["jonny", "thom"]);
+friends.set("thom", []);
+friends.set("jonny", []);
 
-
-export default bfs(friendList:Map<string, string[]>, start:string, isMatch:Function ):string {
+export default function bfs(
+  friendList: Map<string, string[]> = friends,
+  start: string,
+  isMatch: Function
+): string {
   let visited = new Set<string>();
   let queue = new Queue<string>();
   queue.enqueue(start);
-  while (!queue.isEmpty) {
+  while (!queue.isEmpty()) {
+    queue.show();
     let currentFriend = queue.dequeue();
-    if isMatch(currentFriend) {
+    if (isMatch(currentFriend)) {
       return currentFriend;
     } else {
+      visited.add(currentFriend);
       friendList.get(currentFriend).forEach(friend => {
-        queue.enqueue(friend);
-      })
+        if (!visited.has(friend)) {
+          queue.enqueue(friend);
+        }
+      });
     }
   }
-  return 'not found!';
+  return "not found!";
 }
-
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import binarySearch from "./1-binary-search";
+import bfs from "./6-breadth-first-search";
 
 const ns = [0, 2, 5, 8, 9, 23, 56, 78];
 console.log(binarySearch(ns, 8));
 
 const users = ["Alice", "Bob", "Claire", "Di", "Erin", "Farah"];
 console.log(binarySearch(users, "Bob"));
+
+console.log(
+  bfs(undefined, "you", name => name.slice(-1) === "mango".slice(0, 1))
+);
