73c0a1ac27d2dd8889ada5efdf315c9518647d4c
Jan Dierckx <jpuntd@gmail.Com>
2020-06-15 22:12:04 +0200

    add type assertions

--- src/4-quicksort.ts
+++ src/4-quicksort.ts
@@ -2,12 +2,12 @@ export default function quicksort<T>(array: T[]): T[] {
   if (array.length < 2) {
     return array;
   } else {
-    const pivot = array.slice(0, 1);
-    const rest: T[] = array.slice(1);
+    const pivot = array[0];
+    const rest = array.slice(1) as T[];
     return [
       ...quicksort(rest.filter(x => x <= pivot)),
       pivot,
       ...quicksort(rest.filter(x => x > pivot))
-    ];
+    ] as T[];
   }
 }
